ba50cdfc7ae20b3fda7a55242ee8a8320012a240
Dmitry Gridnev <dmitriy.gridnev@outlook.com>
2021-05-25 22:55:19 +0300

    Added Entities

---
+++ ProcessKiller/Entities/Process.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Entities
+{
+    public class Process    
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartTimeOfMonitoring { get; set; }
+    }
+}
